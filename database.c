907a3150c06ab1b588993790846521ca9c8db2bb
Davy <d.vdschoot@gmail.com>
2024-08-04 15:41:47 +0200

    First Commit

---
+++ database.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_EMPLOYEES 1000
+
+struct employees_t { 
+    int id;
+    bool isManager; 
+    char firstName[64];
+    char lastName[64];
+    float income;
+};
+
+void initialize_employees(struct employees_t employees[]) {
+    for(int i = 0; i < MAX_EMPLOYEES; i++) {
+        employees[i].isManager = false;
+        employees[i].income = 0;
+    }
+}
+
+int main() {
+    struct employees_t employees[MAX_EMPLOYEES];
+    initialize_employees(employees);
+}
